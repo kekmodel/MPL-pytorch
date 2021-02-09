a4806b5f9c28f8834f62515af5ed00c31192fb7d
Jungdae Kim <kekmodel@gmail.com>
2021-02-09 15:45:50 +0900

    Test code

--- utils.py
+++ utils.py
@@ -48,13 +48,13 @@ def accuracy(output, target, topk=(1,)):
     maxk = max(topk)
     batch_size = target.shape[0]
 
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
+    _, idx = output.sort(dim=1, descending=True)
+    pred = idx.narrow(1, 0, maxk).t()
     correct = pred.eq(target.reshape(1, -1).expand_as(pred))
 
     res = []
     for k in topk:
-        correct_k = correct[:k].reshape(-1).float().sum(0)
+        correct_k = correct[:k].reshape(-1).float().sum(dim=0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
